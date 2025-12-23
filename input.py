fcfe151e7785854b68c8e7762c0877acb19c727c
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-12-23 17:43:59 +0530

    input.py

---
+++ input.py
@@ -0,0 +1,13 @@
+#INPUT-STATEMENT 
+
+a = float(int(input(" ENTER YOUR NO1: ")))
+
+b = float(int(input(" ENTER YOUR NO2: ")))
+
+print("number a is:",a)
+print("number b is :", b)
+
+print("sum is:", a+b)
+print("sum is:", a-b)
+print("sum is:", a*b)
+print("sum is:", a/b)
