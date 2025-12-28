268983330bf9e4814fd2ecf647b752a36ab8b5b2
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-12-28 15:51:20 +0530

    task2.py

---
+++ task2.py
@@ -0,0 +1,20 @@
+#Task 2: Multiple Print Statements
+#Print the following using print():
+
+'''
+eg. 
+Name: YourName
+Age: YourAge
+City: YourCity
+'''
+
+#variables
+name = "Shristi_Dixit"
+age = 20
+city = "Delhi"
+
+#Print the detail using print statements:
+print(name)
+print(age)
+print(city)
+
