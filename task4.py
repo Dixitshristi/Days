c1647e289452ab7f4a639322824b56264564a483
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-12-28 16:13:36 +0530

    task4.py

---
+++ task4.py
@@ -0,0 +1,15 @@
+'''
+Task 4: Input + Variable
+Take input for:
+roll number
+branch
+Then print:
+Roll No: ___
+branch: ___
+'''
+
+a = int(input("Enter your roll number: "))
+b = input("Enter your branch:")
+
+print("Your roll number is:",a)
+print("Your branch is:",b)
