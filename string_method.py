489c39838da0bb943998d533ea2d01bdf3beebd1
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-12-29 14:17:32 +0530

    string_method.py

---
+++ string_method.py
@@ -0,0 +1,17 @@
+name = "shristi"
+
+print(len(name))
+print(name.endswith("sti"))
+print(name.startswith("shr"))
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.capitalize())
+
+text = "Hi I'm shristi dixit at IITM"
+print(text.find("Hi"))
+print(text.count("i"))
+print(text.strip())
+print(text.replace("I'm","My self"))
+
+
