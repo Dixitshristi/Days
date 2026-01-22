6ce0bb9fadbd2869c2b36968b9ca9ed505592871
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2026-01-22 20:36:59 +0530

    problem_statement_09.py

---
+++ problem_statement_09.py
@@ -0,0 +1,22 @@
+marks =[]
+
+f1 =input("Enter your Marks here is: ")
+marks.append(f1)
+
+f2 =input("Enter your Marks here is: ")
+marks.append(f2)
+
+f3 =input("Enter your Marks here is: ")
+marks.append(f3)
+
+f4 =input("Enter your Marks here is: ")
+marks.append(f4)
+
+f5 =input("Enter your Marks here is: ")
+marks.append(f5)
+
+f6 =input("Enter your Marks here is: ")
+marks.append(f6)
+
+marks.sort()
+print(marks)
