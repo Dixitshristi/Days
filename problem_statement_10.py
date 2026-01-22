5b0ba54bdf5139eba181eda83fb1e4bd5ac6157c
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2026-01-22 20:23:39 +0530

    problem_statement_10.py

---
+++ problem_statement_10.py
@@ -0,0 +1,25 @@
+fruits =[]
+
+f1 =input("ebter your fruit name is: ")
+fruits.append(f1)
+
+f2 =input("ebter your fruit name is: ")
+fruits.append(f2)
+
+f3 =input("ebter your fruit name is: ")
+fruits.append(f3)
+
+f4 =input("ebter your fruit name is: ")
+fruits.append(f4)
+
+f5 =input("ebter your fruit name is: ")
+fruits.append(f5)
+
+f6 =input("ebter your fruit name is: ")
+fruits.append(f6)
+
+f7 =input("ebter your fruit name is: ")
+fruits.append(f7)
+
+
+print(fruits)
