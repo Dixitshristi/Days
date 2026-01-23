2f31ca89890ec8b07895cff9916a73f78f9398ad
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2026-01-23 17:46:31 +0530

    dict4.py

---
+++ dict4.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Alex",
+    "age": 20,
+    "course": "CS"
+}
+
+print(student.keys())    # dict_keys(['name', 'age', 'course'])
+print(student.values())  # dict_values(['Alex', 20, 'CS'])
+print(student.items())   # dict_items([('name', 'Alex'), ('age', 20), ('course', 'CS')
+print(student.update({"name" : "megha" }))
+print(student)
